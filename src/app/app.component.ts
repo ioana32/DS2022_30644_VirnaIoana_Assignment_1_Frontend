34609eb523eb96666a32cf1006787f6ea88beb81
Ioana <i_virna@yahoo.com>
2022-11-06 14:20:51 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'DS2022_30644_VirnaIoana_Assignment_1_Frontend';
+}
